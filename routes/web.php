e81f4772d527b925643157f3bb42a2a9238d8b37
QuantumBen <omosabenedict@gmail.com>
2024-11-14 00:07:25 -0800

    Added route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/fetch-data', [\App\Http\Controllers\ApiController::class, 'fetchData']);
